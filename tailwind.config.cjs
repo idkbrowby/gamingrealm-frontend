e740623ae799b322b92af871da084405c6ba18fa
Gautham Krishna <gautham.jayasurya@gmail.com>
2022-11-01 20:37:52 +0530

    colours and trial

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'lprimary':'#7D6167',
+        'lsecondary':'#FFCFD2',
+        'laccents':'#F7FFDD',
+        'lhighlights':'#D0E3CC',
+        'dprimary':'#011627',
+        'dsecondary':'#b0b7bf',
+        'daccents':'#222e50',
+        'dhighlights':'#7c0b2b',
+      }
+    },
   },
   plugins: [],
 }
