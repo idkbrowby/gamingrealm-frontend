eee23cf9739005ee08b598f8265dad9208c65fa8
Gautham Krishna <gautham.jayasurya@gmail.com>
2022-11-02 12:43:10 +0530

    starting of navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,31 @@
-<div class="mainbg">
-    <div class= "button">
-        <a href="https://google.com">Test</a>
+<script>
+    let lprimary='#7D6167';
+    let lsecondary='#FFCFD2';
+    let laccents='#F7FFDD';
+    let lhighlights='#D0E3CC';
+    let dprimary='#011627';
+    let dsecondary='#b0b7bf';
+    let daccents='#222e50';
+    let dhighlights='#7c0b2b';
+</script>
+<title>Landing page</title>
+<nav class="bg-laccents ">
+    <div class="p-2 border-b-2 border-dprimary">
+        <div class="leftside">
+            <div class="content-center border-pink-300 border-t-2 bg-white " href="./+page.svelte">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="{dhighlights}" class="w-6 h-6" >
+                    <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+                    <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+                  </svg>
+            </div>
+            <div class="content-center border-t-2 border-dprimary bg-dsecondary">
+                <h1 class="text-laccents font-bold items-center">IDKBROWBY</h1>
+            </div>
+        </div>
+        <div class="rightside">
+            <div class="bg-white w-5">
+            </div>
+        </div>
     </div>
-    <div>
-        
-    </div>
-</div>
+    
+</nav>
