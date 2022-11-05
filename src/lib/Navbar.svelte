61e04fc686bb80774e9749451b5451e88c3dd429
Gautham Krishna <gautham.jayasurya@gmail.com>
2022-11-05 14:12:38 +0530

    Mobile menu retraction on size change

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -5,9 +5,10 @@
     let accents='#DA7B93';
     let highlights='#376E6F';
     let menuactive = false;
+    var windowsize = window.innerWidth;
 </script>
 <nav class="w-full mx-auto bg-slate-500 pl-3 pr-3 py-0.5 rounded-b-lg">
-    <div class="flex justify-between items-center h-10 px-2 ">
+    <div class="flex justify-between items-center h-10 px-2 ease-in-out">
         <!-- Logo -->
         <div class=" flex space-x-4 items-center text-white hover:text-primary hover:fill-accents transition duration-200">
             <button class="flex pr-2 transition duration-200">
@@ -47,29 +48,31 @@
     </div>
     <!-- mobile menu -->
     {#if menuactive}
-    <div class="mobile-menu flex-col pt-2 transition duration-200">
-        <!-- searchbar mobile -->
-        <div class=" flex border border-white items-center rounded-xl hover:bg-primary pr-0.5 transition duration-200">
-            <input class="w-full rounded-xl " id="searchbar">
-            <button class="items-center ">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-6 h-6 pr-1 py-0.5">
-                    <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
-                </svg>
-                                 
-            </button>
+        {#if windowsize<768}
+        <div class="mobile-menu flex-col pt-2 md:hidden transition duration-200 ease-in-out screen">
+            <!-- searchbar mobile -->
+            <div class=" flex border border-white items-center rounded-xl  hover:bg-accents transition duration-200 pr-0.5 transition duration-200">
+                <input class="w-full rounded-xl " id="searchbar">
+                <button class="items-center ">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="white" class="w-6 h-6 pr-1 py-0.5">
+                        <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
+                    </svg>
+                                    
+                </button>
+            </div>
+            <!-- Login bar -->
+            <div class=" px-2 py-2 flex justify-between text-white hover:text-pink-400 text-opacity-85">
+                <button class="w-full"><a href="#" class="font-mono bold flex justify-between items-center">Login <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                    <path fill-rule="evenodd" d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z" clip-rule="evenodd" />
+                    <path fill-rule="evenodd" d="M19 10a.75.75 0 00-.75-.75H8.704l1.048-.943a.75.75 0 10-1.004-1.114l-2.5 2.25a.75.75 0 000 1.114l2.5 2.25a.75.75 0 101.004-1.114l-1.048-.943h9.546A.75.75 0 0019 10z" clip-rule="evenodd" />
+                    </svg></a>
+                </button>
+            </div>
+            <!-- Signup Bar -->
+            <div class="px-2 py-2 w-full text-white hover:text-pink-400 text-opacity-85">
+                <button class="flex we-full justify-start"><a href="#" class="font-mono font-medium">Signup</a></button>
+            </div>
         </div>
-        <!-- Login bar -->
-        <div class=" px-2 py-2 flex justify-between text-white hover:text-pink-400 text-opacity-85">
-            <button class="w-full"><a href="#" class="font-mono bold flex justify-between items-center">Login <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                <path fill-rule="evenodd" d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z" clip-rule="evenodd" />
-                <path fill-rule="evenodd" d="M19 10a.75.75 0 00-.75-.75H8.704l1.048-.943a.75.75 0 10-1.004-1.114l-2.5 2.25a.75.75 0 000 1.114l2.5 2.25a.75.75 0 101.004-1.114l-1.048-.943h9.546A.75.75 0 0019 10z" clip-rule="evenodd" />
-                </svg></a>
-            </button>
-        </div>
-        <!-- Signup Bar -->
-        <div class="px-2 py-2 flex justify-between text-white hover:text-pink-400 text-opacity-85">
-            <a href="#" class="font-mono bold">Signup </a>
-        </div>
-    </div>
+        {/if}
     {/if}
 </nav>
