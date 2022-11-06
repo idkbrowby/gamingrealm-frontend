669c95bbc416346b20a7d263699b8ccd22444234
Gautham Krishna <gautham.jayasurya@gmail.com>
2022-11-06 11:03:42 +0530

    Default password removed

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     let agreedTnC = false;
-    let pass="1234";
-    let confpass="1234";
+    let pass;
+    let confpass;
 </script>
 <div class="min-h-screen bg-slate-800 flex p-20 justify-center">
     <div class="flex items-center justify-center">
@@ -37,7 +37,7 @@
                     </div>
                     <div>
                     {#if (pass!=confpass)}
-                        <h1 class="pb-2 text-white">Passwords Do Not Match!!</h1>
+                        <h1 class="pb-2 text-white text-mono">Passwords Do Not Match!!</h1>
                         <button type="submit" disabled class="text-white bg-pink-400 hover:bg-pink-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center disabled:bg-slate-500">Submit</button>
                     {:else}
                         <button type="submit" disabled={!agreedTnC} class="text-white bg-pink-400 hover:bg-pink-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center disabled:bg-slate-500">Submit</button>
