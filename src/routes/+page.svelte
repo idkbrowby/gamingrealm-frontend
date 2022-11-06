e27d3e5b4094a3566c10032f117b500e4ad66082
Gautham Krishna <gautham.jayasurya@gmail.com>
2022-11-06 15:56:34 +0530

    Navbar User added and Signup Page done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
 	import Layout from "./+layout.svelte";
-    import Signup from "./signup/+page.svelte"
+    import Login from "./login/+page.svelte";
+    import Signup from "./signup/+page.svelte";
     import Navbar from "$lib/Navbar.svelte";
 </script>
-<body class="bg-primary">
+<body class="bg-slate-500">
     
 </body>
-<Navbar />
-<Signup />
+<Navbar/>
 
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -9,10 +9,6 @@
                 <h1 class="text-lg py-10 basis-1/6 md:text-5xl font-mono font-extrabold text-start text-pink-400 ">Signup</h1>
                 <form>
                     <div class="py-2">
-                        <label for="Name" class="block mb-2 text-sm font-medium font-mono text-gray-300">Name</label>
-                        <input type="text" id="Name" class="bg-gray-50 border font-mono text-gray-100 text-sm rounded-lg focus:ring-pink-300 focus:border-pink-500 block w-full p-2.5 dark:bg-gray-700 border-gray-600" placeholder="John" required>
-                    </div>
-                    <div class="py-2">
                         <label for="Emailid" class="block mb-2 text-sm font-medium font-mono text-gray-300">Email Id</label>
                         <input type="text" id="Emailid" class="bg-gray-50 border font-mono text-gray-100 text-sm rounded-lg focus:ring-pink-300 focus:border-pink-500 block w-full p-2.5 dark:bg-gray-700 border-gray-600" placeholder="John24123@gmail.com" required>
                     </div>
@@ -26,7 +22,7 @@
                     </div>
                     <div class="py-2">
                         <label for="password" class="block mb-2 text-sm font-medium font-mono text-gray-300">Confirm Password</label>
-                        <input type="password" id="password" class="bg-gray-50 border font-mono text-gray-100 text-sm rounded-lg focus:ring-pink-300 focus:border-pink-500 block w-full p-2.5 dark:bg-gray-700 border-gray-600" bind:value={confpass} placeholder="Password " required>
+                        <input type="password" id="password" class="bg-gray-50 border font-mono text-gray-100 text-sm rounded-lg focus:ring-pink-300 focus:border-pink-500 block w-full p-2.5 dark:bg-gray-700 border-gray-600" bind:value={confpass} placeholder="Password" required>
                     </div>
                     
                     <div class="flex items-start mb-6 py-4">
@@ -35,15 +31,17 @@
                         </div>
                         <label for="remember" class="ml-2 text-sm font-mono font-medium text-gray-900 dark:text-gray-400">I agree with the <a href="/" class="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
                     </div>
-                    <div>
                     {#if (pass!=confpass)}
-                        <h1 class="pb-2 text-white text-mono">Passwords Do Not Match!!</h1>
+                    <div class="pb-2">
+                        <h1 class=" text-white text-mono">Passwords Do Not Match!!</h1>
+                    </div>
                         <button type="submit" disabled class="text-white bg-pink-400 hover:bg-pink-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center disabled:bg-slate-500">Submit</button>
                     {:else}
                         <button type="submit" disabled={!agreedTnC} class="text-white bg-pink-400 hover:bg-pink-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center disabled:bg-slate-500">Submit</button>
                     {/if}
-                    </div>
+                    <p class="py-2 font-mono text-pink-200">Already a User? <a class=" hover:text-slate-100"href="login/+page.svelte">Login Here</a> <p>
                 </form>
         </div>
     </div>
+        
 </div>
