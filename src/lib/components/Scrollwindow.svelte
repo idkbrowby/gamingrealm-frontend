ce08c13b6cc0274f9c9db13341dbfa9331dbad84
Gautham Krishna <gautham.jayasurya@gmail.com>
2022-11-09 22:00:32 +0530

    background colors chanegd

--- src/lib/components/Scrollwindow.svelte
+++ src/lib/components/Scrollwindow.svelte
@@ -28,19 +28,21 @@
     ...newBatch
   ];
 </script>
-
-<div class="h-full items-center py-10 px-4 w-full flex justify-between text-white font-mono">
-    <div class=" flex w-full justify-between px-2 space-x-4">
+<body class="h-full">
+<div class="h-full items-center py-4 px-4 w-full flex justify-between text-white font-mono ">
+    <div class=" flex w-full justify-between space-x-4  ">
         
-        <div class="w-full border-2  flex-col justify-center border-white rounded-xl space-y-2 p-2">
-            <div class="w-full border-2 flex-col justify-center border-white rounded-xl" id="createpost">
-                Create post
-            </div>
-            <div class="w-full p-2 border-2 border-white">
-                <ul class="max-h-60 overflow-hidden overflow-y-auto space-y-1 text-white font-mono">   
+        <div class="w-full flex-col justify-center rounded-xl space-y-2 bg-slate-800 p-4">
+            <button class="w-full">
+                <div class="w-full border-2 py-2 flex-col justify-center border-white rounded-lg" id="createpost">
+                    Create post
+                </div>
+            </button>
+            <div class="w-full p-2 border-r-2 border-t-2 border-l-2 rounded-xl border-white">
+                <ul class="max-h-60 overflow-auto space-y-1 text-white font-mono">   
                         {#each data as post}
                             <li> 
-                                <div class=" p-1 items-center border-2 border-white space-y-1">
+                                <div class=" p-1 items-center border-2 rounded-md border-white space-y-1">
                                     {post.name}
                                 </div> 
                             </li>
@@ -52,10 +54,11 @@
                 </ul>
             </div>
         </div>
-        <div class="hidden md:flex p-2 border-2 border-white rounded-xl">
+        <div class="hidden md:flex space-x-16 bg-slate-800 p-4 rounded-xl">
             test
         </div>
         
     </div>
     
 </div>
+</body>
