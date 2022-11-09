c722a749d195e96a7c6e0268a9889cca9cc2982b
Gautham Krishna <gautham.jayasurya@gmail.com>
2022-11-09 22:54:11 +0530

    Userpage updated

--- src/lib/components/UserPage.svelte
+++ src/lib/components/UserPage.svelte
@@ -13,28 +13,76 @@
     let parsedCreatedAt = DateTime.fromISO(createdAt);
 </script>
 
-<div class="flex items-center rounded-xl bg-slate-700">
-    <h1 class="text-2xl md:text-4xl font-mono font-semibold text-slate-50">{username}</h1>
-    <p class="font-mono text-start text-slate-50">Email: {email}</p>
-    <p class="font-mono text-start text-slate-50">Joined: {parsedCreatedAt.toLocaleString(DateTime.DATETIME_FULL)}</p>
-    <span class="inline-flex">
-        <p class="font-mono text-slate-50 text-center"><bold class="text-bold">{followerCount}</bold> followers</p>
-        <p class="font-mono text-slate-50 text-center"><bold class="text-bold">{followingCount}</bold> following</p>
-        <p class="font-mono text-slate-50 text-center"><bold class="text-bold">{postsCount}</bold> posts</p>
-    </span>
-    {#if $page.data.user && username != $page.data.user.username}
-    <!--User is logged in and is not viewing their own page-->
-        <span class="inline-flex">
-            {#if currentUserIsFollowing}
-                <button
-                class="bg-primary hover:bg-orange-200 hover:bg-opacity-75 transition duration-200 border-4 border-primary rounded-xl p-1 text-white hover:text-primary  flex items-center"
-                ><a href="/unfollow" class=" font-mono  font-semibold ">Unfollow</a></button>
-            {:else}
-                <button
-                class="bg-primary hover:bg-orange-200 hover:bg-opacity-75 transition duration-200 border-4 border-primary rounded-xl p-1 text-white hover:text-primary  flex items-center"
-                ><a href="/follow" class=" font-mono  font-semibold ">Follow</a></button
-            >
-            {/if}
+<div class="p-2">
+    <div class="flex-col items-center rounded-xl bg-slate-700 p-2">
+        <h1 class="flex justify-center text-2xl md:text-4xl font-mono font-semibold text-slate-50">{username}</h1>
+        <p class="flex justify-center font-mono text-start text-slate-50">Email: {email}</p>
+        <p class="flex justify-center font-mono text-start text-slate-50">Joined: {parsedCreatedAt.toLocaleString(DateTime.DATETIME_FULL)}</p>
+        <span class="flex justify-center space-x-10">
+            <div class="flex-col ">
+                <div>
+                    <a class=" font-mono text-slate-50 text-3xl text-center" href="/followers">
+                        <button>
+                            <bold class="text-bold">{followerCount}</bold>
+                        </button>
+                    </a>
+                </div>
+                <div>
+                    <a class=" font-mono text-slate-50 text-center" href="/followers">
+                        <button>
+                            <bold class="text-bold">Followers</bold>
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <div class="flex-col ">
+                <div>
+                    <a class=" font-mono text-slate-50 text-3xl text-center" href="/followers">
+                        <button>
+                            <bold class="text-bold">{followingCount}</bold>
+                        </button>
+                    </a>
+                </div>
+                <div>
+                    <a class=" font-mono text-slate-50 text-center" href="/followers">
+                        <button>
+                            <bold class="text-bold">Following</bold>
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <div class="flex-col ">
+                <div>
+                    <a class=" font-mono text-slate-50 text-3xl text-center" href="/followers">
+                        <button>
+                            <bold class="text-bold">{postsCount}</bold>
+                        </button>
+                    </a>
+                </div>
+                <div>
+                    <a class=" font-mono text-slate-50 text-center" href="/followers">
+                        <button>
+                            <bold class="text-bold">Posts</bold>
+                        </button>
+                    </a>
+                </div>
+            </div>
+            
         </span>
-    {/if}
+        {#if $page.data.user && username != $page.data.user.username}
+        <!--User is logged in and is not viewing their own page-->
+            <span class="inline-flex">
+                {#if currentUserIsFollowing}
+                    <button
+                    class="bg-primary hover:bg-orange-200 hover:bg-opacity-75 transition duration-200 border-4 border-primary rounded-xl p-1 text-white hover:text-primary  flex items-center"
+                    ><a href="/unfollow" class=" font-mono  font-semibold ">Unfollow</a></button>
+                {:else}
+                    <button
+                    class="bg-primary hover:bg-orange-200 hover:bg-opacity-75 transition duration-200 border-4 border-primary rounded-xl p-1 text-white hover:text-primary  flex items-center"
+                    ><a href="/follow" class=" font-mono  font-semibold ">Follow</a></button
+                >
+                {/if}
+            </span>
+        {/if}
+    </div>
 </div>
