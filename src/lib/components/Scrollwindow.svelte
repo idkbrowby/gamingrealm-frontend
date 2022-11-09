16277427fca789e9b39997da289354f4b8425686
Gautham Krishna <gautham.jayasurya@gmail.com>
2022-11-09 12:17:51 +0530

    basic layout for scrollwindow

---
+++ src/lib/components/Scrollwindow.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+</script>
+
+<div class="h-full items-center py-10 px-4 w-full flex justify-between">
+    <div class=" flex w-full justify-between px-2 space-x-4">
+        
+        <div class="w-full border-2 flex-col justify-center border-white rounded-xl space-y-2 p-2">
+            <div class="w-full border-2 flex-col justify-center border-white rounded-xl" id="createpost">
+                Create post
+            </div>
+            <div class="w-full border-2 flex-col justify-center border-white rounded-xl">
+                <div class="flex flex-wrap justify-between" id="postdetails">
+
+                </div>
+            </div>
+        </div>
+        <div class="hidden md:flex p-2 border-2 border-white rounded-xl">
+            test
+        </div>
+        
+    </div>
+    
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Navbar from '$lib/components/Navbar.svelte';
+	import Scrollwindow from '$lib/components/Scrollwindow.svelte';
 </script>
 
 <body class="bg-slate-500" />
 <Navbar />
+<Scrollwindow/>
