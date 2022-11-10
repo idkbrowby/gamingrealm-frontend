c6ec0e6d6fa53493a06d2da88e0fd7dd00e2b041
Gautham Krishna <gautham.jayasurya@gmail.com>
2022-11-10 18:21:01 +0530

    small changes

--- src/lib/components/Scrollwindow.svelte
+++ src/lib/components/Scrollwindow.svelte
@@ -12,8 +12,10 @@
 	// store the new batch of data here.
 	let newBatch = [];
 	
+    let tags=["asd","Asdasd","asdasdasd","asdasdasdqwerlia","iuoqweoiasdflhasdfj","aksujjhaksjdhaksjdh"];
+
 	async function fetchData() {
-		const response = await fetch(`https://api.openbrewerydb.org/breweries?by_city=los_angeles&page=${page}`);
+		const response = await fetch(`https://api.openbrewerydb.org/breweries?page=${page}`);
 		newBatch = await response.json();
 		console.log(newBatch)
 	};
@@ -28,37 +30,52 @@
     ...newBatch
   ];
 </script>
-<body class="h-full">
-<div class="h-full items-center py-4 px-4 w-full flex justify-between text-white font-mono ">
+<div class=" py-4 px-4 w-full flex justify-between text-white font-mono ">
     <div class=" flex w-full justify-between space-x-4  ">
         
         <div class="w-full flex-col justify-center rounded-xl space-y-2 bg-slate-800 p-4">
-            <button class="w-full">
-                <div class="w-full border-2 py-2 flex-col justify-center border-white rounded-lg" id="createpost">
-                    Create post
-                </div>
-            </button>
-            <div class="w-full p-2 border-r-2 border-t-2 border-l-2 rounded-xl border-white">
-                <ul class="max-h-60 overflow-auto space-y-1 text-white font-mono">   
+            <a href="/createpost">
+                <button class="w-full">
+                    <div class="w-full border-2 py-2 flex-col justify-center border-white rounded-lg" id="createpost">
+                        Create post
+                    </div>
+                </button>
+            </a>
+            <!--Posts -->
+            <div class="w-full p-2 overflow-y-auto border-r-2 border-t-2 border-l-2 rounded-xl border-white">
+                <ul class=" overflow-y-auto space-y-1 text-white font-mono ">   
                         {#each data as post}
                             <li> 
                                 <div class=" p-1 items-center border-2 rounded-md border-white space-y-1">
                                     {post.name}
                                 </div> 
+                                <div class=" p-1 items-center border-2 rounded-md border-white space-y-1">
+                                    {post.state}
+                                </div> 
+                                <div class=" p-1 items-center border-2 rounded-md border-white space-y-1">
+                                    {post.longitude}
+                                </div> 
                             </li>
                         {/each}
                         <InfiniteScroll
                             hasMore={newBatch.length}
-                            threshold={100}
+                            threshold={1000}
                             on:loadMore={() => {page++; fetchData()}} />
+                            {page}
                 </ul>
             </div>
         </div>
-        <div class="hidden md:flex space-x-16 bg-slate-800 p-4 rounded-xl">
-            test
+        <div class="hidden md:flex flex-col space-x-16 bg-slate-800 p-4 items-center rounded-xl">
+            <h1>Tags</h1>
+            <div class = "flex flex-wrap space-y-4 justify-center">
+                {#each tags as tag}
+                    <div class=" border-2 border-white">
+                        {tag}
+                    </div>
+                {/each}
+            </div>
         </div>
         
     </div>
     
 </div>
-</body>
