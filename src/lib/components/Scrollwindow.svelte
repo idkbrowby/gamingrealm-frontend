7d87f8b65c73f8479ff071b8040de87f781badae
anand2312 <40204976+anand2312@users.noreply.github.com>
2022-11-11 00:13:48 +0530

    Fix infinite scroll

--- src/lib/components/Scrollwindow.svelte
+++ src/lib/components/Scrollwindow.svelte
@@ -70,7 +70,7 @@
                         <InfiniteScroll
                         hasMore={newBatch.length}
                         threshold={100}
-                        on:loadMore={() => {cursorid=fetchData()}} />
+                        on:loadMore={() => {cursorid = fetchData()}} />
                     </ul>
                 </div>
             </div>
