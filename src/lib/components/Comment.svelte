0e4a7ba9370db1708ad023ac6a2d690095efabeb
Gautham Krishna <gautham.jayasurya@gmail.com>
2022-11-11 04:25:18 +0530

    Comment window layout added

--- src/lib/components/Comment.svelte
+++ src/lib/components/Comment.svelte
@@ -1,3 +1,23 @@
-<script>
+<script lang="ts">
     
+    import { DateTime } from "luxon";
+    export let username:string;
+    export let comment :string;
+    export let created_at: string;
 </script>
+<div class="flex flex-col ">
+    <div class="flex justify-between">
+        <!-- Username -->
+        <div class="flex font-semibold text-xl">
+            {username}
+        </div>
+        <!-- Date -->
+        <div class="flex font-semibold font-xl">
+            {created_at}
+        </div>
+    </div>
+    <!-- Comment body -->
+    <div>
+        {comment}
+    </div>
+</div>
--- src/lib/components/Postpage.svelte
+++ src/lib/components/Postpage.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { DateTime } from "luxon";
     import InfiniteScroll from "./InfiniteScroll.svelte";
+    import Comment from "./Comment.svelte";
     export let title:string;
     export let author:string;
     export let rating : number;
@@ -8,7 +9,7 @@
     export let image : string; 
 </script>
 <div class="w-full h-auto flex border-2 border-white py-8 md:px-20 px-10">
-    <div class="flex flex-col w-full  border-2 justify-start p-2 bg-slate-800 border-white rounded-xl">
+    <div class="flex flex-col w-full  border-2 justify-start p-4 bg-slate-800 border-white rounded-xl space-y-4">
         <div class="flex flex-col bg-slate-500 p-4 rounded-xl">
             <!-- Author -->
             <div class="p-2 font-mono font-bold text-xl">
@@ -37,8 +38,8 @@
             </div>
         </div>
         <!-- Comments -->
-        <div class="bg-slate-500" >
-            
+        <div class="bg-slate-500 rounded-xl p-4 font-mono" >
+            <Comment username="User" comment=" Lorem ipsum dolor sit amet, consectetur adipiscing elit." created_at="10/20/2021"/>
         </div>
     </div>
 </div>
