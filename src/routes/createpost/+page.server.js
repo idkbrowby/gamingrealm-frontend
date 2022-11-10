6ebe017ea4c4d82bb4d61df32ae68e459fd3c1ce
Gautham Krishna <gautham.jayasurya@gmail.com>
2022-11-10 12:19:00 +0530

    Added create post page with tags and item input

---
+++ src/routes/createpost/+page.server.js
@@ -0,0 +1,7 @@
+import { invalid, redirect } from '@sveltejs/kit';
+
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ locals }) {
+	if (locals.user) throw redirect(307, '/login');
+}
---
+++ src/routes/kundi/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+import Tags from "svelte-tags-input";
+
+// If on:tags is defined
+let tag = "";
+
+function handleTags(event) {
+    tag = event.detail.tags;
+}
+
+const countryList = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "American Samoa",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antarctica",
+    "Antigua and Barbuda",
+    "Argentina",
+    "India"
+];
+</script>
+<Tags
+    on:tags={handleTags}
+    addKeys={[9]} 
+    maxTags={3}
+    allowPaste={true}
+    allowDrop={true}
+    splitWith={"/"}
+    onlyUnique={true}
+    removeKeys={[27]} 
+    placeholder={"Svelte Tags Input full example"}
+    autoComplete={countryList}
+    name={"custom-name"}
+    id={"custom-id"}
+    allowBlur={true}
+    disable={false}
+    minChars={3}
+    onlyAutocomplete
+    labelText="Label"
+    labelShow
+/>
