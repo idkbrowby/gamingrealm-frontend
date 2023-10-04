6e4460dce976cfb4242fca7a7a9ce836ab831d2d
Gautham Krishna <gautham.jayasurya@gmail.com>
2023-10-04 14:23:19 +0530

    hooks changed to ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,8 @@
+import type { App } from './app';
+
 /** @type {import('@sveltejs/kit').Handle} */
-export function handle({ event, resolve }) {
+export const handle = ({ event, resolve }) => {
 	const user = event.cookies.get('user');
 	event.locals.user = user ? JSON.parse(atob(user)) : null;
 	return resolve(event);
-}
+};
