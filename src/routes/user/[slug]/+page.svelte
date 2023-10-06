c5fdf76993f22df9dd767ed67384cb1d5269c039
Gautham Krishna <gautham.jayasurya@gmail.com>
2023-10-06 16:44:42 +0530

    updated basic user page

---
+++ src/routes/user/[slug]/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import User from '$lib/components/User.svelte';
+	import BasicNavbar from '$lib/components/BasicNavbar.svelte';
+	export let data;
+</script>
+
+<div class="h-full w-full">
+	<BasicNavbar />
+	<div class="py-4 px-2 flex justify-center items-center">
+		<div class="lg:w-1/2">
+			<User
+				username={data.username}
+				follower_count={data.follower_count}
+				following_count={data.following_count}
+				since={data.created_at}
+				posts_count={data.posts_count}
+			/>
+		</div>
+	</div>
+</div>
