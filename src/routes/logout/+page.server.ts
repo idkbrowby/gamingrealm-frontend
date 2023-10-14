f285b0d3ddc0436c5b3007e2357d3f99629110d9
Gautham Krishna <gautham.jayasurya@gmail.com>
2023-10-14 10:46:52 +0530

    cookie deletion on error

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -6,14 +6,16 @@ export async function load({ cookies, locals }) {
 		const user = locals.user ? JSON.parse(atob(cookies.get('user'))) : null;
 		const sessionid = user.data ? user.data['session-id'] : null;
 		const response = await api.logout(sessionid);
+		cookies.delete('user');
+		locals.user = null;
 		if (response.error) {
 			console.log(response.error);
 			throw redirect(307, '/');
 		}
 		console.log(response.data);
-		cookies.delete('user');
-		locals.user = null;
 		throw redirect(307, '/');
 	}
+	cookies.delete('user');
+	locals.user = null;
 	throw redirect(307, '/');
 }
