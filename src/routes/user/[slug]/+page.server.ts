faac8dc7b2b2d1a704b6887c6d47a51636d9a7bf
Gautham Krishna <gautham.jayasurya@gmail.com>
2023-10-14 10:48:32 +0530

    Basic User page done

--- src/routes/user/[slug]/+page.server.ts
+++ src/routes/user/[slug]/+page.server.ts
@@ -1,20 +1,25 @@
 import { error } from '@sveltejs/kit';
 import * as api from '$lib/api';
+
 export async function load({ params, locals }) {
 	if (
 		/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(params.slug)
 	) {
-		if (locals) {
+		if (locals && locals.user) {
 			const { session_id, user_id } = {
-				session_id: locals.user.data['session-id'],
+				session_id: locals.session_id,
 				user_id: params.slug
 			};
-			const res = await api.get_user(user_id, locals.session_id);
+			const res = await api.get_user(user_id, session_id);
 			if (res.error) {
 				// throwing error for user not found
 				throw error(404, 'User Not Found');
 			} else {
-				return res.data;
+				if (res.data) {
+					res.data['sameuser'] = locals.user.data.user.id === user_id;
+
+					return res.data;
+				}
 			}
 		}
 	}
--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -7,14 +7,21 @@
 <div class="h-full w-full">
 	<BasicNavbar />
 	<div class="py-4 px-2 flex justify-center items-center">
-		<div class="lg:w-full w-2/3">
+		<div class="lg:w-full md:w-4/5 w-full">
 			<User
 				username={data.username}
 				follower_count={data.follower_count}
 				following_count={data.following_count}
 				since={data.created_at}
 				posts_count={data.posts_count}
+				is_following={data.is_following}
+				sameuser={data['sameuser']}
 			/>
 		</div>
 	</div>
+	{#if data.posts_count == 0}
+		<div class="w-full flex items-center justify-center">
+			<strong>This User has no Posts</strong>
+		</div>
+	{/if}
 </div>
