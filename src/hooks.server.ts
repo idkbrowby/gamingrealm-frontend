9d4b44eac90746c474b7309ee47308d40cab1274
Gautham Krishna <gautham.jayasurya@gmail.com>
2023-10-14 10:48:11 +0530

    type errors fixed in hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,14 @@
 import type { App } from './app';
+import type { RequestEvent } from '@sveltejs/kit';
+interface UserLocals extends RequestEvent {
+	locals: {
+		user: App.Locals['user'] | null;
+	};
+}
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle = ({ event, resolve }) => {
 	const user = event.cookies.get('user');
-	event.locals.user = user ? JSON.parse(atob(user)) : null;
+	(event as UserLocals).locals.user = user ? JSON.parse(atob(user)) : null;
 	return resolve(event);
 };
