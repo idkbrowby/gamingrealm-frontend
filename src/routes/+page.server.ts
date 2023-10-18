5a29d2bc6cc19bf306b73435ee1b883eee8f3219
Gautham Krishna <gautham.jayasurya@gmail.com>
2023-10-18 11:51:12 +0530

    placeholder on main page for testing

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,10 @@
 import type { App } from '../app';
+
 export async function load({ locals }: { locals: App.Locals }) {
 	if (locals && locals.user) {
+		const user = locals.user.data.user;
 		return {
-			user: locals.user.data.user
+			user: user
 		};
 	}
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Post from '$lib/components/post.svelte';
+	import Loadingpost from '$lib/components/loadingpost.svelte';
 </script>
 
 <div class="h-full w-full flex-col space-y-6">
 	<div><Navbar /></div>
 
 	<div class="h-full w-full flex justify-center">
-		<div class="w-full">
+		<div class="flex-col flex w-full gap-2">
 			<Post title="title" rating={4} description="description" author="author" />
+			<Loadingpost />
 		</div>
 	</div>
 </div>
