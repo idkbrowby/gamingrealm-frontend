d1444d447ddd690ba10bc2d318a25aec9467275a
Gautham Krishna <gautham.jayasurya@gmail.com>
2023-10-18 13:47:39 +0530

    fixing post component

--- src/lib/components/post.svelte
+++ src/lib/components/post.svelte
@@ -12,7 +12,7 @@
 <div class="flex-col p-2 w-full bg-surface-800-100-token text-surface-100-800-token">
 	<div class="flex my-2 w-full justify-between">
 		<div>
-			<a href="/profile/{author_id}">
+			<a href="/user/{author_id}">
 				<div class="flex gap-2 items-center">
 					<div><Avatar {initials} class="h-8 w-8" /></div>
 					<div>{author}</div>
