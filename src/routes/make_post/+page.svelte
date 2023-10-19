d76ab763a5f316eaf8340a4e2d7ed7d3d9e502df
Gautham Krishna <gautham.jayasurya@gmail.com>
2023-10-19 21:54:48 +0530

    make_post route basics done

---
+++ src/routes/make_post/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import BasicNavbar from '$lib/components/BasicNavbar.svelte';
+	import Makepost from '$lib/components/makepost.svelte';
+</script>
+
+<BasicNavbar />
+<Makepost />
