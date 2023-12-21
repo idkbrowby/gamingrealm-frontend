06328e6bcd7fc043384810b64a0ad40d5616b0a6
Gautham Krishna <gautham.jayasurya@gmail.com>
2023-12-21 10:13:40 +0530

    better error pages

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,12 +5,18 @@
 
 <div class="h-screen w-screen">
 	<div class="flex items-center justify-center h-full">
-		<h1 class="text-3xl font-light mt-1">{x === undefined ? 404 : x}</h1>
-		<h1 class="mx-2 font-medium text-7xl mb-2">|</h1>
-		<div class="flex items-center justify-center p-1">
-			<h1 class="text-5xl text-bold text-error-600">
-				{$page.error.message.toUpperCase()}
-			</h1>
-		</div>
+		<a href="/feed">
+			<button>
+				<div class="flex items-center justify-center h-full w-full">
+					<h1 class="text-3xl font-light mt-1">{x === undefined ? 404 : x}</h1>
+					<h1 class="mx-2 font-medium text-7xl mb-2">|</h1>
+					<div class="flex items-center justify-center p-1">
+						<h1 class="text-5xl text-bold text-error-600">
+							{$page.error.message.toUpperCase()}
+						</h1>
+					</div>
+				</div>
+			</button>
+		</a>
 	</div>
 </div>
